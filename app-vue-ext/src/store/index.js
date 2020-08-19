474bd0a185f93866c0895c98a98b2f28f82a52d5
zdm <zdm@softvisio.net>
2020-08-19 22:53:28 +0300

    users store

--- app-vue-ext/src/store/index.js
+++ app-vue-ext/src/store/index.js
@@ -1,17 +1,10 @@
 import Store from "#softvisio/store";
 
 // import CONST from "@/const";
-import UserModel from "@/models/user";
 
 export default class extends Store {
     state () {
-        return {
-            "userStore": Ext.create( "Ext.data.Store", {
-                "model": UserModel,
-                "autoLoad": false,
-                "pageSize": 50,
-            } ),
-        };
+        return {};
     }
 
     getters () {
