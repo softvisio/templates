e875cf537cd969580410e431630a95b028f62b25
zdm <zdm@softvisio.net>
2020-11-21 19:19:17 +0200

    api call log endpoint added

---
+++ server/lib/api/v1/admin/api-call-log.js
@@ -0,0 +1,10 @@
+const { mix } = require( "@softvisio/core/mixins" );
+const Base = require( "@softvisio/core/app/mixins/base" );
+const ApiCallLog = require( "@softvisio/core/app/mixins/admin/api-call-log" );
+
+/** class: ApiCallLog
+ * summary: App API call log.
+ * extends:
+ *   - '@softvisio/core/app/mixins/admin/api-call-log#ApiCallLog'
+ */
+module.exports = class extends mix( ApiCallLog, Base ) {};
