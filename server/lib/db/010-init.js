eb167d562257f68637ec6f76f2f980da59b3365b
zdm <zdm@softvisio.net>
2020-11-26 23:50:31 +0200

    pgsql extensions

--- server/lib/db/010-init.js
+++ server/lib/db/010-init.js
@@ -2,9 +2,9 @@ const sql = require( "@softvisio/core/sql" );
 
 module.exports = sql`
 
--- CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
--- CREATE EXTENSION IF NOT EXISTS "timescaledb" CASCADE;
+CREATE EXTENSION IF NOT EXISTS "timescaledb" CASCADE;
 
 -- CREATE EXTENSION IF NOT EXISTS "pg_hashids";
 
