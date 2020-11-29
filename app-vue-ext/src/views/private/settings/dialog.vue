e5b188b6aca94456b0eab048c2d4822dbaaf98be
zdm <zdm@softvisio.net>
2020-11-29 07:28:41 +0200

    settings panel

--- app-vue-ext/src/views/private/settings/dialog.vue
+++ app-vue-ext/src/views/private/settings/dialog.vue
@@ -1,7 +1,7 @@
 <template>
     <Dialog>
         <!-- <ext&#45;fieldset slot="top" title="General Settings" defaults='{"labelAlign":"left","labelWidth":250}'> -->
-        <!--     <ext&#45;textfield label="Google Maps API Key" bind="{record.gmap_key}"/> -->
+        <!--     <ext&#45;textfield label="Test Field" bind="{record.test_field}"/> -->
         <!-- </ext&#45;fieldset> -->
     </Dialog>
 </template>
