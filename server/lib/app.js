3a94e04cfbc17d4d017a2fb4991e6a676dba1bcf
zdm <zdm@softvisio.net>
2020-12-31 00:11:02 +0200

    app api builder

--- server/lib/app.js
+++ server/lib/app.js
@@ -37,7 +37,7 @@ module.exports = class extends App {
         this.#dbh = sql.connect( process.env.APP_DB );
 
         // create api endpoint
-        this.#api = await this._buildApi( this.#dbh, null, {
+        this.#api = await this._buildApi( this.#dbh, {
             "permissions": CONST.PERMISSIONS,
             "schema": __dirname + "/db",
             "methods": __dirname + "/api",
