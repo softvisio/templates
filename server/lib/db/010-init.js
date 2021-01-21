4413dfbaa5c174e5bed5963c3238a83aadb9b155
zdm <zdm@softvisio.net>
2021-01-21 20:20:59 +0200

    db types template

--- server/lib/db/010-init.js
+++ server/lib/db/010-init.js
@@ -2,14 +2,14 @@ const sql = require( "@softvisio/core/sql" );
 
 module.exports = {
     "sql": sql`
+
+        -- EXTENSIONS
         CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
         CREATE EXTENSION IF NOT EXISTS "timescaledb" CASCADE;
-
         -- CREATE EXTENSION IF NOT EXISTS "pg_hashids";
 
         -- TYPES
-        -- CREATE DOMAIN "currency" AS decimal(12, 4) NULL DEFAULT 0
+        -- CREATE DOMAIN "currency" AS decimal(12, 4) NOT NULL DEFAULT 0
     `,
     "types": {
 
