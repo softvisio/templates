a8fc0cb4972fdc1ca0e8d641fb4dca64c6c1f692
zdm <zdm@softvisio.net>
2021-04-01 21:11:35 +0300

    app settings dialog updated

--- app-vue-ext/src/views/private/settings/dialog.vue
+++ app-vue-ext/src/views/private/settings/dialog.vue
@@ -1,9 +1,11 @@
 <template>
     <SettingsDialog>
         <template #top>
-            <!-- <ext&#45;fieldset title="General Settings" defaults='{"labelAlign":"left","labelWidth":250}'> -->
-            <!--     <ext&#45;textfield label="Test Field" bind="{record.test_field}"/> -->
-            <!-- </ext&#45;fieldset> -->
+            <!-- <ext&#45;panel title="General Settings"> -->
+            <!--     <ext&#45;fieldset title="General Settings" defaults='{"labelAlign":"left","labelWidth":250}'> -->
+            <!--         <ext&#45;textfield label="Test Field" bind="{record.test_field}"/> -->
+            <!--     </ext&#45;fieldset> -->
+            <!-- </ext&#45;panel> -->
         </template>
     </SettingsDialog>
 </template>
