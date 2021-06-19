4b4b1f074074e782961e4394c4c4840255e61dda
zdm <zdm@softvisio.net>
2021-06-19 21:42:22 +0300

    app api / rpc init code changed

--- server/lib/app.js
+++ server/lib/app.js
@@ -5,8 +5,6 @@ import CONST from "#lib/const";
 
 export default class extends App {
     #dbh;
-    #api;
-    #rpc;
 
     static cli () {
         return {
@@ -50,20 +48,20 @@ export default class extends App {
         // create dbh
         this.#dbh = await sql.connect( process.env.APP_DB );
 
-        // create api endpoint
-        this.#api = await this.API.new( this, this.#dbh, {
+        // create api
+        res = await this.initAPI( this.API, this.#dbh, {
             "permissions": CONST.PERMISSIONS,
             "objects": CONST.OBJECTS,
             "dbSchema": new URL( "./db", import.meta.url ),
             "apiSchema": new URL( "./api", import.meta.url ),
         } );
-        if ( !this.#api ) return result( 500 );
+        if ( !res.ok ) return res;
 
-        // create rpc endpoint
-        // this.#rpc = await this.RPC.new( this, {
-        //     "apiSchema": new URL( "./rpc", import.meta.url ),
-        // } );
-        // if ( !this.#rpc ) return result( 500 );
+        // create rpc
+        res = await this.initRPC( this.RPC, {
+            "apiSchema": new URL( "./rpc", import.meta.url ),
+        } );
+        if ( !res.ok ) return res;
 
         // run threads
         process.stdout.write( "Starting threads ... " );
@@ -71,22 +69,22 @@ export default class extends App {
             "worker": {
                 "num": 1,
                 "path": new URL( "./threads/worker.js", import.meta.url ),
-                "arguments": [this.#api.settings],
+                "arguments": [this.api.settings],
             },
         } );
         console.log( res + "" );
         if ( !res.ok ) return res;
 
         // create HTTP server locations
-        this.server.webpack( "/", new URL( "../app/www", import.meta.url ) ).api( "/api", this.#api );
+        this.server.webpack( "/", new URL( "../app/www", import.meta.url ) ).api( "/api", this.api );
 
         // run HTTP server
         res = await this.server.listen( "0.0.0.0", 80 );
         if ( !res.ok ) return res;
 
         // run RPC service
-        // res = await this.cluster.listen( this.#rpc );
-        // if ( !res.ok ) throw res;
+        res = await this.cluster.listen( this.rpc );
+        if ( !res.ok ) throw res;
 
         return result( 200 );
     }
