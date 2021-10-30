6c005ee4e1697425beb44bb0cdfba7bd4447664a
zdm <zdm@softvisio.net>
2021-10-30 09:51:28 +0300

    feat: sql int53 type

--- server/lib/db/10-init.js
+++ server/lib/db/10-init.js
@@ -6,8 +6,19 @@ export default sql`
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 -- CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
--- types
--- CREATE DOMAIN currency AS decimal ( 12, 4 ) NOT NULL DEFAULT 0;
+-- int53
+-- DO $$ BEGIN
+--     IF to_regtype( 'int53' ) IS NULL THEN
+--         CREATE DOMAIN IF NOT EXISTS int53 AS int8 CHECK ( VALUE >= -9007199254740991 AND VALUE <= 9007199254740991 );
+--     END IF;
+-- END $$;
+
+-- currency
+-- DO $$ BEGIN
+--     IF to_regtype( 'int53' ) IS NULL THEN
+--         CREATE DOMAIN currency AS decimal ( 12, 4 ) NOT NULL DEFAULT 0;
+--     END IF;
+-- END $$;
 
 `;
 
