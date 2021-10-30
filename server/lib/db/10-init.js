b079f7066af028f69aeeaf6a2fbae1f7e4458453
zdm <zdm@softvisio.net>
2021-10-30 15:31:54 +0300

    fix: api int53

--- server/lib/db/10-init.js
+++ server/lib/db/10-init.js
@@ -6,13 +6,6 @@ export default sql`
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 -- CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
--- int53
--- DO $$ BEGIN
---     IF to_regtype( 'int53' ) IS NULL THEN
---         CREATE DOMAIN IF NOT EXISTS int53 AS int8 CHECK ( VALUE >= -9007199254740991 AND VALUE <= 9007199254740991 );
---     END IF;
--- END $$;
-
 -- currency
 -- DO $$ BEGIN
 --     IF to_regtype( 'int53' ) IS NULL THEN
