84981cbb631cbe537cf6c83e3e3678a917239886
zdm <zdm@softvisio.net>
2022-02-12 20:53:47 +0200

    fix: webpack config

--- app-vue/webpack.config.js
+++ app-vue/webpack.config.js
@@ -1,5 +1,5 @@
-import config from "#vue/webpack.config.vue-app";
+import config from "#vue/resources/webpack.vue.app.config.js";
 
-config.schemas.push( new URL( "./resources/schemas/env.vue-app.schema.yaml", import.meta.url ) );
+config.schemas.push( new URL( "./resources/schemas/env.vue.app.schema.yaml", import.meta.url ) );
 
 export default [config];
