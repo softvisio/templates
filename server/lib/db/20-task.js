ecb75a0bd9fea2de5853fec45f831f0950e80deb
zdm <zdm@softvisio.net>
2022-03-18 20:06:24 +0200

    fix: const

--- server/lib/db/20-task.js
+++ server/lib/db/20-task.js
@@ -1,12 +1,12 @@
 import sql from "#core/sql";
-import CONST from "#lib/const";
+import constrants from "#lib/app.config";
 
 export default sql`
 
 CREATE SEQUENCE task_id_seq AS int8 MAXVALUE 36028797018963967;
 
 CREATE TABLE task (
-    id int8 PRIMARY KEY NOT NULL DEFAULT gen_object_id( ${CONST.objects.task.id}, 'task_id_seq' )
+    id int8 PRIMARY KEY NOT NULL DEFAULT gen_object_id( ${constrants.objects.task.id}, 'task_id_seq' )
 );
 
 CREATE TRIGGER task_after_delete AFTER DELETE ON task FOR EACH ROW EXECUTE PROCEDURE object_after_delete_trigger();
