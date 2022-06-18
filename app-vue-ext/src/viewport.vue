20b05746ad8c71c2ab3e345f2badd9008c2fe0d2
zdm <zdm@softvisio.net>
2022-06-19 02:43:23 +0300

    fix: webpack chunk names

--- app-vue-ext/src/viewport.vue
+++ app-vue-ext/src/viewport.vue
@@ -6,8 +6,8 @@ export default {
     "extends": Viewport,
 
     created () {
-        this.publicView = defineAsyncComponent( () => import( /* webpackChunkName: "private" */ "@/views/public.vue" ) );
-        this.privateView = defineAsyncComponent( () => import( /* webpackChunkName: "public" */ "@/views/private.vue" ) );
+        this.publicView = defineAsyncComponent( () => import( /* webpackChunkName: "public" */ "@/views/public.vue" ) );
+        this.privateView = defineAsyncComponent( () => import( /* webpackChunkName: "private" */ "@/views/private.vue" ) );
     },
 
     "methods": {
