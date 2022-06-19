16427aea7b2c4469e933801748b5fe58def95507
zdm <zdm@softvisio.net>
2022-06-19 04:17:10 +0300

    fix: webpack chunks

--- app-vue/src/locale.js
+++ app-vue/src/locale.js
@@ -2,4 +2,4 @@ import locale from "#vue/locale";
 
 export default locale;
 
-await locale.add( language => import( "#resources/locales/" + language + ".po" ) );
+await locale.add( language => import( /* webpackMode: "eager" */ "#resources/locales/" + language + ".po" ) );
