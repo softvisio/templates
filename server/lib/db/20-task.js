1a02c961c463ef827f072c15af965a5e2e2d4416
zdm <zdm@softvisio.net>
2022-07-15 00:57:36 +0300

    fix: prinamry key fixed

--- server/lib/db/20-task.js
+++ server/lib/db/20-task.js
@@ -6,7 +6,7 @@ export default sql`
 CREATE SEQUENCE task_id_seq AS int8 MAXVALUE 36028797018963967;
 
 CREATE TABLE task (
-    id int8 PRIMARY KEY NOT NULL DEFAULT gen_object_id( ${constrants.objects.task.id}, 'task_id_seq' )
+    id int8 PRIMARY KEY DEFAULT gen_object_id( ${constrants.objects.task.id}, 'task_id_seq' )
 );
 
 CREATE TRIGGER task_after_delete AFTER DELETE ON task FOR EACH ROW EXECUTE PROCEDURE object_after_delete_trigger();
