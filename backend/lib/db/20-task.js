7fee6b9002f6bcd00f4a2d857a1e48674ba39eac
zdm <zdm@softvisio.net>
2022-07-27 11:25:30 +0300

    fix: sql procedure renamed to function

--- backend/lib/db/20-task.js
+++ backend/lib/db/20-task.js
@@ -9,6 +9,6 @@ CREATE TABLE task (
     id int8 PRIMARY KEY DEFAULT gen_object_id( ${constrants.objects.task.id}, 'task_id_seq' )
 );
 
-CREATE TRIGGER task_after_delete AFTER DELETE ON task FOR EACH ROW EXECUTE PROCEDURE object_after_delete_trigger();
+CREATE TRIGGER task_after_delete AFTER DELETE ON task FOR EACH ROW EXECUTE FUNCTION object_after_delete_trigger();
 
 `;
