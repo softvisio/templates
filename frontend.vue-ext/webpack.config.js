a41ff9c5a686f70ba4cda9113c5d48dad076c612
zdm <zdm@softvisio.net>
2022-08-24 12:56:56 +0300

    fix: app env schema

--- frontend.vue-ext/webpack.config.js
+++ frontend.vue-ext/webpack.config.js
@@ -1,5 +1,5 @@
 import config from "#vue/resources/webpack/default.config.js";
 
-config.main.schemas.push( new URL( "./resources/schemas/env.vue.app.schema.yaml", import.meta.url ) );
+config.main.schemas.push( new URL( "./resources/schemas/env.schema.yaml", import.meta.url ) );
 
 export default config;
