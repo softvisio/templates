fb450f96aeba2231314bf65b8c67696af8fddeba
zdm <zdm@softvisio.net>
2022-08-30 03:11:08 +0300

    fix: webpack config refactored

--- frontend.vue-ext/webpack.config.js
+++ frontend.vue-ext/webpack.config.js
@@ -1,5 +1,5 @@
 import config from "#vue/resources/webpack/default.config.js";
 
-config.main.schemas.push( new URL( "./resources/schemas/app.env.schema.yaml", import.meta.url ) );
+config.get( "main" ).addSchema( "#resources/schemas/app.env.schema.yaml", import.meta.url );
 
 export default config;
