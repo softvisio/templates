c203a4c69766baf26d11e6b92ad4b8978b6af218
zdm <zdm@softvisio.net>
2022-09-05 19:54:23 +0300

    fix: firebase worker

--- frontend.vue-ext/src/firebase-messaging.worker.js
+++ frontend.vue-ext/src/firebase-messaging.worker.js
@@ -1,1 +1,10 @@
-export default Super => class extends ( Super || Object ) {};
+export default Super =>
+    class extends ( Super || Object ) {
+        async onBackgoundMessage ( message ) {
+            return super.onBackgoundMessage( message );
+        }
+
+        async onClick ( event ) {
+            return super.onClick( event );
+        }
+    };
