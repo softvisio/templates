d10ffc47fc916430169ba46ada4012f35b8e1038
zdm <zdm@softvisio.net>
2022-09-06 19:17:08 +0300

    fix: api key changed to token

--- backend/lib/api/v1/account/api-keys.js
+++
@@ -1,3 +0,0 @@
-import ApiKeys from "#core/app/prototypes/account/api-keys";
-
-export default class extends ApiKeys {}
---
+++ backend/lib/api/v1/account/tokens.js
@@ -0,0 +1,3 @@
+import ApiTokens from "#core/app/prototypes/account/tokens";
+
+export default class extends ApiTokens {}
