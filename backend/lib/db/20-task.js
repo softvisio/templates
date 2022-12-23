396f726baf62405e16abe4542dbb2c0f3adb87ad
zdm <zdm@softvisio.net>
2022-12-23 06:40:58 +0200

    fix: templates

--- backend/lib/db/20-task.js
+++ backend/lib/db/20-task.js
@@ -2,10 +2,10 @@ import sql from "#core/sql";
 
 export default sql`
 
-CREATE TABLE task (
-    id int8 PRIMARY KEY DEFAULT gen_acl_id( 'task-type' )
-);
+-- CREATE TABLE task (
+--     id int8 PRIMARY KEY DEFAULT create_acl( 'task-type' )
+-- );
 
-CREATE TRIGGER task_after_delete AFTER DELETE ON task FOR EACH ROW EXECUTE FUNCTION acl_after_delete_trigger();
+-- CREATE TRIGGER task_after_delete AFTER DELETE ON task FOR EACH ROW EXECUTE FUNCTION acl_after_delete_trigger();
 
 `;
