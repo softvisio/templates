c6b23793770d44bcce90df973d1dc55c51b2cac4
zdm <zdm@softvisio.net>
2022-12-23 19:26:17 +0200

    fix: app run threads

--- backend/lib/app.js
+++ backend/lib/app.js
@@ -14,20 +14,23 @@ export default class extends App {
         res = await super._init();
         if ( !res.ok ) return res;
 
-        // res = await this.threads.run( {
-        //     "worker": {
-        //         "num": 1,
-        //         "module": new URL( "./threads/worker.js", import.meta.url ),
-        //         "arguments": null,
-        //     },
-        // } );
-        // if ( !res.ok ) return res;
-
         return result( 200 );
     }
 
     async _run () {
         return super._run();
+    }
+
+    async _runThreads () {
+        return super._runThreads();
+
+        // return this.threads.run( {
+        //     "worker": {
+        //         "num": 1,
+        //         "module": new URL( "threads/worker.js", import.meta.url ),
+        //         "arguments": null,
+        //     },
+        // } );
     }
 
     async _getHealthCheckStatus () {
