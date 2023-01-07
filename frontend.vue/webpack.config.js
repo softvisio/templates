b8edf09d9b65fa4e06632810c7215a69cc6047e6
zdm <zdm@softvisio.net>
2023-01-07 10:54:44 +0200

    fix: webpack config refactored

--- frontend.vue/webpack.config.js
+++ frontend.vue/webpack.config.js
@@ -1,11 +1,1 @@
 export * from "#vue/resources/webpack/default.config.js";
-
-import { Main as Super } from "#vue/resources/webpack/default.config.js";
-
-export class Main extends Super {
-    constructor ( options ) {
-        super( options );
-
-        this.addSchema( "#resources/schemas/app.env.schema.yaml", import.meta.url );
-    }
-}
