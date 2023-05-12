f519cf52e7214034b9525ced87965ee68f0f76fd
zdm <zdm@softvisio.net>
2023-05-12 16:31:45 +0300

    fix: webpack compoent validator

--- frontend.vue/resources/webpack-components/firebase-messaging-worker/index.js
+++ frontend.vue/resources/webpack-components/firebase-messaging-worker/index.js
@@ -1,3 +1,9 @@
 import WebpackComponent from "@softvisio/vue/webpack-components/firebase-messaging-worker";
 
-export default class extends WebpackComponent {}
+export default class extends WebpackComponent {
+
+    // public
+    validateEnv ( env ) {
+        return super.validateEnv( env ) || this._validateEnv( env, import.meta.url );
+    }
+}
--- frontend.vue/resources/webpack-components/main/index.js
+++ frontend.vue/resources/webpack-components/main/index.js
@@ -2,13 +2,8 @@ import WebpackComponent from "@softvisio/vue/webpack-components/main";
 
 export default class extends WebpackComponent {
 
-    // properties
-    get schemas () {
-        return [
-
-            //
-            ...super.schemas,
-            new URL( "env.schema.yaml", import.meta.url ),
-        ];
+    // public
+    validateEnv ( env ) {
+        return super.validateEnv( env ) || this._validateEnv( env, import.meta.url );
     }
 }
