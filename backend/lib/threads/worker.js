e55ab386240cbbfcbb642177a200e26623841182
zdm <zdm@softvisio.net>
2023-06-04 17:09:40 +0300

    fix: health check

--- backend/lib/threads/worker.js
+++ backend/lib/threads/worker.js
@@ -1,18 +1,7 @@
-import Thread from "#core/app/thread";
-
-export default class extends Thread {
+export default class {
 
     // public
     async API_test () {
         return result( 200 );
-    }
-
-    // protected
-    async _init ( options = {} ) {
-        if ( options.dbh ) {
-            await this._initDbh( options.dbh );
-        }
-
-        return super._init( options );
     }
 }
